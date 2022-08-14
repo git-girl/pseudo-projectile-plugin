744969e212e63344a082ac35f7a6721dc1db7f3d
Lotta <lotta.stefanova@posteo.de>
2022-08-14 19:45:02 +0200

    can esc fzf without nvim in $HOME but not in other dirs

--- pseudo-projectile.plugin.zsh
+++ pseudo-projectile.plugin.zsh
@@ -6,8 +6,11 @@ PATHTOPROJECTS="$HOME/projects"
 
 # open project symlink folder with FZF and start nvim
 open_project() { 
+    start_dir=$PWD 
     cd $(find -L $PATHTOPROJECTS -maxdepth 1 -type d -print | fzf --no-multi --height 30%)
-    nvim .
+    if [[ $PWD != $start_dir ]] then 
+        nvim . 
+    fi
 } 
 
 # add project (PWD to project symlink folder) 
