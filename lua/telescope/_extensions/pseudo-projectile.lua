d46eb5cd47557422dbaff5742cfd9719447b90a0
Lotta <lotta.stefanova@posteo.de>
2022-08-16 22:19:58 +0200

    refactor back into one method with flags

---
+++ lua/telescope/_extensions/pseudo-projectile.lua
@@ -0,0 +1,10 @@
+-- https://github.com/nvim-telescope/telescope.nvim/blob/master/developers.md#bundling-as-extension
+return require("telescope").register_extension {
+  setup = function(ext_config, config)
+    -- access extension config and user config
+  end,
+  exports = {
+      -- this is the problem line 
+    openProjects = require("pseudo-projectile").openProjects
+  },
+}
--- pseudo-projectile.plugin.zsh
+++ pseudo-projectile.plugin.zsh
@@ -5,7 +5,7 @@
 PATHTOPROJECTS="$HOME/projects"
 
 # open project symlink folder with FZF and start nvim
-open_project() { 
+project_go() { 
 
     start_dir=$PWD 
 
@@ -14,12 +14,26 @@ open_project() {
 
     # only open if there is some cd path selected 
     if [[ $cd_path != "" ]] then
-        nvim . 
+        cd $cd_path
     fi
 } 
 
+# weird naming but better as gp will be aliased alot 
+project_edit() {  
+
+    start_dir=$PWD 
+
+    #set the path to cd into and perform checks
+    cd_path=$(find -L $PATHTOPROJECTS -maxdepth 1 -type d -print | fzf --no-multi --height 30%)
+
+    # only open if there is some cd path selected 
+    if [[ $cd_path != "" ]] then
+        cd $cd_path
+        nvim . 
+    fi
+} 
 # add project (PWD to project symlink folder) 
-add_project() { 
+project_add() { 
     ln -s $PWD $PATHTOPROJECTS
     echo "added $PWD to projects"
 } 
