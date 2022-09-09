b7bcadd3afff11435d878dc5ea274426c2e9a828
Lotta <lotta.stefanova@posteo.de>
2022-09-09 20:03:17 +0200

    Refactored into Options, Supporting e -> editor & s -> size fzf

--- pseudo-projectile.plugin.zsh
+++ pseudo-projectile.plugin.zsh
@@ -4,37 +4,57 @@
 
 # open project symlink folder with FZF and start nvim
 
+# TODO: Showing Path should be an Option OR RATHER HOW MUCH OF PATH
+
 project_open() { 
 
-    start_dir=$PWD 
+    local start_dir=$PWD 
+    # default size 10 Percent
+    local size=10
+
+    while getopts "e:s:" opt; do 
+        case "${opt}" in
+            e)
+                local editor=${OPTARG} # should return stuff like nvim, vim, code
+                ;;
+            s) 
+                local size=${OPTARG}
+                echo $size
+                ;;
+            h) 
+                echo "this should return a usage out"
+                return
+                ;;
+            :) 
+                echo "this should return a usage out"
+                echo "TODO: this needs to abort fzf" 
+                ;;
+            ?) 
+                echo "must supply an argumen" 
+                echo "TODO: this needs to abort fzf" 
+                ;;
+        esac
+    done
 
     #set the path to cd into and perform checks
-    cd_path=$(find -L $PATHTOPROJECTS -maxdepth 1 -print | fzf --no-multi --height 30%)
+    local cd_path=$(find -L $PATHTOPROJECTS -maxdepth 1 -print | fzf --no-multi --height $size)
 
     # only open if there is some cd path selected 
     if [[ $cd_path != "" ]] then
         # TODO: if $cd_path.is_a(Directory) elif cd_path.is_a(File) -> go to directory in file path before it
         cd $cd_path
+        if (( ${+editor} )); then 
+            $editor . 
+        fi
     fi
 } 
 
-# weird naming but better as gp will be aliased alot 
-project_edit() {  
-
-    start_dir=$PWD 
-
-    #set the path to cd into and perform checks
-    cd_path=$(find -L $PATHTOPROJECTS -maxdepth 1 -print | fzf --no-multi --height 30%)
-
-    # only open if there is some cd path selected 
-    if [[ $cd_path != "" ]] then
-        cd $cd_path
-        nvim . 
-    fi
-} 
 # add project (PWD to project symlink folder) 
 project_add() { 
     ln -s $PWD $PATHTOPROJECTS
     echo "added $PWD to projects"
 } 
 
+# TODO: there should be some way to call an alias like po and pass the typed stuff before entering 
+# as a search into fzf if there is only one entry returned open it
+#
