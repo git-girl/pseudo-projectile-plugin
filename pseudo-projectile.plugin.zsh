d52707a0c3a4204e5a5d761eb479cb15ec8945a1
git_girl <lotta.stefanova@posteo.de>
2023-05-16 21:44:13 +0200

    i think its working ;-;

--- pseudo-projectile.plugin.zsh
+++ pseudo-projectile.plugin.zsh
@@ -60,10 +60,7 @@ project_open() {
         # BUG: If no password this results into uncallable git threads
         # NOTE: Can you just make this a function and then disown that?
 
-        if git_check_read_origin; then 
-          echo "started git check"
-          run_and_report_git_diff & disown
-        fi
+        git_main & disown
       fi
       if (( ${+editor} )); then 
         $editor . 
@@ -79,17 +76,37 @@ project_add() {
 
 git_check_read_origin_and_fetch() { 
   # returns 124 on timeout to $?
-  timeout 3 git fetch >/dev/null & disown
-  if [[ $? == 124 ]]; then 
+  # returns 127 on command not known to $?
+  timeout 3 git fetch 
+
+  retval=$?
+
+  if [[ $retval != 0 ]]; then 
     notify-send "PPP Git Report" "couldn't run git fetch (3sec timeout), check your ssh keys, or internet connection"
-    return 124
   fi
-  return 0
+
+  return $retval
 }
 
 run_and_report_git_diff() { 
-  git_diff=$(git diff @{u} HEAD --name-only)
-  notify-send "PPP Git Report" "Found these files on origin not present locally: \n $git_diff"
 
-          # git diff @{u} HEAD --name-only | xargs -r notify-send "PPP: G it Report" ""  & disown
+  git_diff=$(git diff @{u} HEAD --name-only)
+  if [[ $git_diff != "" ]]; then
+    notify-send "PPP Git Report" "Found these files on origin not present locally: \n $git_diff"
+  else 
+    notify-send "PPP Git Report" "Clean: no diff to origin isn't ahead of local"
+  fi  
+}
+
+git_main() { 
+  git_check_read_origin_and_fetch
+
+  access=$?
+
+  echo "access exit code is: $access"
+
+  if [ $access -eq 0 ]; then 
+    echo "started git check"
+    run_and_report_git_diff & disown
+  fi
 }
